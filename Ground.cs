41659ea45f1a109688db5b5dbbfefb78b4a8ad9a
LordofMadness <zemekoulak@gmail.com>
2023-03-01 16:45:03 +0100

    adding what should be essential files.

---
+++ Ground.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class Ground : TileMap
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	public override void _Ready()
+	{
+		
+	}
+	
+
+}
