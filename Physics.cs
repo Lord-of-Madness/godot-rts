370eb7f5326c18bca5c0aa37bf4182b7023020e5
LordofMadness <zemekoulak@gmail.com>
2023-03-21 22:31:55 +0100

    Adding Physics namespace to simplify work on movement and other physics related things.

---
+++ Physics.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+namespace RtsZápočťák
+{
+    public struct TilesPerSecond
+    {
+        private float value;
+        public TilesPerSecond(float value)
+        {
+            this.value=value*16;
+        }
+        public static explicit operator TilesPerSecond(float value)
+        {
+            return new TilesPerSecond(value);
+        }
+        public static TilesPerSecond operator *(TilesPerSecond tps,float mult)
+        {
+            return new TilesPerSecond(tps.value*mult);
+        }
+        public static Vector2 operator*(TilesPerSecond tps,Vector2 vec)
+        {
+            return new Vector2(vec*tps.value);
+        }
+    }
+}
