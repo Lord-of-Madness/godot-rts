19f3499da20205bc17314510833baa3dbed9e128
LordofMadness <zemekoulak@gmail.com>
2023-07-07 20:07:41 +0200

    Removing unnecessary variables

--- scripts/SelectRect.cs
+++ scripts/SelectRect.cs
@@ -3,22 +3,21 @@ namespace RTSUI
 {
     public partial class SelectRect : Node2D
     {
-
-        private Vector2 start;
+        public Color color = new(0, 1, 0, 0.3f);//Probably alterable in settings eventually
+        public Vector2 start;
         private Vector2 end;
-        private bool dragging;
+        public bool dragging =false;
+        public Vector2 Size { get => (end - start).Abs(); }
         public override void _Draw()
         {
             if (dragging)
             {
-                DrawRect(new Rect2(start, end - start).Abs(), new Color(0, 1, 0, 0.3f));
+                DrawRect(new Rect2(start, end - start).Abs(), color);
             }
         }
-        public void UpdateStats(Vector2 start, Vector2 end, bool dragging)
+        public void UpdateStats( Vector2 end)
         {
-            this.start = start;
             this.end = end;
-            this.dragging = dragging;
             QueueRedraw();
         }
     }
