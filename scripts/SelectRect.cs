00fd3b2b48088691d9e3b9550be09bdf820372b5
LordofMadness <zemekoulak@gmail.com>
2023-07-07 19:16:30 +0200

    Updated to G4 and currently bug free Tilesets and tilemaps completely redone due to engine upgrade A bit of refactoring.

--- scripts/SelectRect.cs
+++ scripts/SelectRect.cs
@@ -1,24 +1,25 @@
 using Godot;
-using System;
-
-public partial class SelectRect : Node2D
+namespace RTSUI
 {
+    public partial class SelectRect : Node2D
+    {
 
-    private Vector2 start;
-    private Vector2 end;
-    private bool dragging;
-    public override void _Draw()
-    {
-        if (dragging)
+        private Vector2 start;
+        private Vector2 end;
+        private bool dragging;
+        public override void _Draw()
         {
-            DrawRect(new Rect2(start,end-start),new Color(0,1,0,0.3f));
+            if (dragging)
+            {
+                DrawRect(new Rect2(start, end - start).Abs(), new Color(0, 1, 0, 0.3f));
+            }
         }
-    }
-    public void UpdateStats(Vector2 start,Vector2 end,bool dragging)
-    {
-        this.start=start;
-        this.end=end;
-        this.dragging=dragging;
-        Update();
+        public void UpdateStats(Vector2 start, Vector2 end, bool dragging)
+        {
+            this.start = start;
+            this.end = end;
+            this.dragging = dragging;
+            QueueRedraw();
+        }
     }
 }
