3d1e19883aae65d3aea9f62f8deef6950e3bd149
LordofMadness <zemekoulak@gmail.com>
2024-03-09 23:51:40 +0100

    Adding boundaries for position

--- scripts/Gameplay/AbilityPair.cs
+++ scripts/Gameplay/AbilityPair.cs
@@ -5,8 +5,8 @@ namespace RTS.Gameplay
     [GlobalClass]
     public partial class AbilityPair : Resource//Dictionaries export badly, also has to be a Resource to be exportable
     {
-        [Export]
-        public int pos;
+        [Export(PropertyHint.Range,"0,23,1")]
+        public ushort pos;
         [Export]
         public AbilityRes ability;
     }
