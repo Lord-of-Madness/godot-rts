c8e1bfb5ae1e5383b1ee1beb044e32861f86f41e
LordofMadness <zemekoulak@gmail.com>
2024-03-12 14:18:24 +0100

    Adding more functionality to TargetedAbility abstract and virtual functions and properties that will be needed/useful

--- scripts/Gameplay/Ability.cs
+++ scripts/Gameplay/Ability.cs
@@ -1,4 +1,4 @@
-using Godot;
+﻿using Godot;
 using RTS.Physics;
 using System;
 
@@ -7,15 +7,61 @@ namespace RTS.Gameplay
     /// <summary>
     /// <para>Signifies if the Ability has to have a target.</para>
     /// <para>I am currently assuming this is a binary state and there are only Targeted/NonTargeted Abilities</para>
+    /// Every child specifies
+    /// 1.what happens when the target is recieved (if we store the target in any way is up to it)
+    /// 2.What happens when the target is reached
     /// </summary>
-    [GlobalClass]
-    public abstract partial class TargetedAbility : Ability//TODO: add range from which it can be triggered.
+    public abstract partial class TargetedAbility : Ability
     {
+        protected Area2D detectionZone;
+        float range  = 0;
+        /// <summary>
+        /// Returns range with the added size of the Selectable ownign it.
+        /// </summary>
+        public float Range { get=>
+                range 
+                + OwningSelectable.SizeRadious//adding size of the Selectable
+                + 14//A hair extra so it works why fourteen? Cause 4 was not good enough and unit increases seemed too weak
+                ; }
         /// <summary>
         /// Triggered upon specifiing the target (Ability.OnClick has been triggered and then a Target has been given)
         /// </summary>
         /// <param name="target"></param>
-        public abstract void OnTargetRecieved(Target target);
+        public virtual void OnTargetRecieved(Target target)
+        {
+            if (OwningSelectable.Position.DistanceTo(target.Position) <= Range) OnTargetReached();//If it is within range already
+            else detectionZone.Monitoring = true;
+        }
+        /// <summary>
+        /// Triggers upon getting within abilityrange of the Target
+        /// </summary>
+        public virtual void OnTargetReached() {
+            if (!Channeled) detectionZone.Monitoring = false;
+        }//I chose not to disable monitoring here cause sometimes an ability might want to be channeled.
+        public override void _Ready()
+        {
+            base._Ready();
+            detectionZone = new();
+            detectionZone.AddChild
+                (new CollisionShape2D()
+                {
+                    Shape = new CircleShape2D()
+                    {
+                        Radius = Range
+                    }
+                }
+                );
+            AddChild(detectionZone);
+            detectionZone.Monitoring = false;//we don't want to use it if it is not necessary (if we ímplement autocast then it might wanna be changed)
+            detectionZone.BodyEntered+=(shape)=> { if (IsTarget(shape)) OnTargetReached(); };
+        }
+        /// <summary>
+        /// Check whether an object that entered the Abilities radious is desired target
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        protected abstract bool IsTarget(Node2D shape);
+
     }
     public abstract partial class Ability : Node
     {
@@ -33,7 +79,7 @@ namespace RTS.Gameplay
         public override void _Process(double delta)
         {
             base._Process(delta);
-            if(cooldown>0) cooldown -= delta;
+            if (cooldown > 0) cooldown -= delta;
             else
             {
                 cooldown = 0f;
@@ -54,13 +100,17 @@ namespace RTS.Gameplay
         /// <summary>
         /// True if Ability is cooling down
         /// </summary>
-        public bool OnCooldown { get
+        public bool OnCooldown
+        {
+            get
             {
                 if (Cooldown == 0) return false;
                 else return cooldown > 0;
-            } }
+            }
+        }
         ///<summary> Describes whether ability is Active or Passive (button is either enabled or not)</summary>
         public abstract bool Active { get; }
+        public bool Channeled { get; set; } = false;//Whole different rabbithole. Gotta check for bodyexits etc.
         /// <summary>
         /// The selectable that owns this Ability
         /// </summary>
