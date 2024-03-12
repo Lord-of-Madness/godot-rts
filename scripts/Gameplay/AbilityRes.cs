b4e068552362df3ff25bfdd549191ca351173d6f
LordofMadness <zemekoulak@gmail.com>
2024-03-12 14:17:23 +0100

    Separating TargetedAbilityRes from AbilityRes to avoid GlobalClass issues

--- scripts/Gameplay/AbilityRes.cs
+++ scripts/Gameplay/AbilityRes.cs
@@ -2,7 +2,7 @@
 
 namespace RTS.Gameplay
 {
-    [GlobalClass]//Will throw errors in Editor but it must be here so those who inherit from this can use it
+    [GlobalClass]
     public abstract partial class AbilityRes : Resource
     {
         public abstract string Text { get; set; }
---
+++ scripts/Gameplay/TargetedAbilityRes.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace RTS.Gameplay
+{
+    [GlobalClass]
+    public abstract partial class TargetedAbilityRes : AbilityRes
+    {
+        [Export(PropertyHint.Range, "0,10,1,or_greater")]
+        public abstract float Range { get; set; }
+
+        public override abstract TargetedAbility Instantiate(Selectable owner);
+    }
+}
+
