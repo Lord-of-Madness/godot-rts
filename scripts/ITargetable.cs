84048abbc90775103a0849c011586d137095fe2d
LordofMadness <zemekoulak@gmail.com>
2024-03-17 01:44:12 +0100

    Moving things to own files

---
+++ scripts/ITargetable.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+
+namespace RTS.Gameplay
+{
+    public interface ITargetable//Interface might be better than a class ->TODO conisder
+    {
+        public Vector2 Position { get; }
+        public string ToString();
+        //[Signal] public delegate void SignalBeingDisposedOfEventHandler(ITargetable targetable); Waiting for pull request to happen
+    }
+}
---
+++ scripts/Location.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+
+namespace RTS.Gameplay
+{
+    public struct Location : ITargetable
+    {
+        private Vector2 data;
+        public readonly Vector2 Position => data;
+        public Location(Vector2 v)
+        {
+            data = v;
+        }
+        public static implicit operator Vector2(Location location) => location.data;
+        public static implicit operator Location(Vector2 v) => new(v);
+        public override readonly string ToString() =>data.ToString();
+
+    }
+}
